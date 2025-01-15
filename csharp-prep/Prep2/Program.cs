14b9dc2f56f070d02e4fb7fe11400df53d2ceb0d
ttunquist1 <tun21002@byui.edu>
2025-01-15 15:39:10 -0700

    Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Create a grading program
+        Console.WriteLine("Student grade");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
