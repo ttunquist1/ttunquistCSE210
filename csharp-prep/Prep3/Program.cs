cbad3c1c99ea5570c5da6637c2ffd656e9724f59
ttunquist1 <tun21002@byui.edu>
2025-01-15 16:02:17 -0700

    Prep 3 done with stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Create a guess the number game
+        bool running = true;
+        List<int> guesses = new List<int>();
+        int magicNumber = new Random().Next(1, 101);
+
+        while (running)
+        {
+            Console.WriteLine("\nWhat is your guess? "); 
+            int guess = int.Parse(Console.ReadLine());
+
+            // Add guess to a list for tracking
+            
+            guesses.Add(guess);
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                running = false;
+            }
+        }
+
+        // Tell User how many guesses they took
+        Console.WriteLine($"\nYou took {guesses.Count} guesses.");
+
+        // Do they want to play again?
+        Console.WriteLine("\nWould you like to play again? (y/n)");
+        string playAgain = Console.ReadLine();
+        if (playAgain == "y")
+        {
+            // Play again
+            Main(args);
+        }
+        
     }
 }
