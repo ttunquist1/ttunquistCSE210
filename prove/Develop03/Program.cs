e5297763ac2b01d8825031a94865bd1e9e940cfb
ttunquist1 <tun21002@byui.edu>
2025-02-14 23:28:10 -0700

    changed verse

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // perform full code
-        Scripture scripture = new Scripture(new Reference("Almla", 18, 27, 27), "27 And he said, Yea.");
+        Scripture scripture = new Scripture(new Reference("Alma", 7, 11, 12), "11 And he shall go forth, suffering pains and afflictions and temptations of every kind; and this that the word might be fulfilled which saith he will take upon him the pains and the sicknesses of his people. 12 And he will take upon him death, that he may loose the bands of death which bind his people; and he will take upon him their infirmities, that his bowels may be filled with mercy, according to the flesh, that he may know according to the flesh how to succor his people according to their infirmities.");
         Console.Clear();
             scripture.Display(); 
             Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
